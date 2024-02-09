908d952a5e3bc7fc53576c9e6690c4fb1d6f37c0
Doug Goldstein <doug.goldstein@rackspace.com>
2024-02-09 16:59:31 -0600

    fix: certain tr's will read forever
    Avoid letting tr read forever from /dev/urandom

--- scripts/pwgen.sh
+++ scripts/pwgen.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/sh -e
 
-LC_ALL=C tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c${1:-32}
+export LC_ALL=C
+dd bs=512 if=/dev/urandom count=1 | tr -dc _A-Z-a-z-0-9 | head -c${1:-32}
 echo
