9133b41f88e2265a7431c3d385d56c562fc4fc89
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-04-02 17:47:40 +0100

    build container images for workflows automatically

--- argo-workflows/ironic-nautobot-sync/container/build_images.sh
+++
@@ -1,2 +0,0 @@
-docker build -t registry.sno.rackspace.net/undercloud/argo-ironic:0.0.1 -f Dockerfile.ironic .
-kind --name understack load docker-image registry.sno.rackspace.net/undercloud/argo-ironic:0.0.1
