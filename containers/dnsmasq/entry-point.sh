3c4868c20edbc5ca186bbb534c68b0ddb5714a28
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-04-11 13:17:37 +0100

    dnsmasq: add config debugging

--- containers/dnsmasq/entry-point.sh
+++ containers/dnsmasq/entry-point.sh
@@ -10,6 +10,10 @@ mkdir -p /etc/dnsmasq.d/optsdir.d
 
 render_j2_file /etc/dnsmasq.conf.j2 /etc/dnsmasq.conf
 
+if [ -n "${DEBUG_DNSMASQ_CONF+x}" ]; then
+  cat /etc/dnsmasq.conf
+fi
+
 /usr/sbin/dnsmasq --test
 
 case "$1" in
