1746dd68c95c47e3189c40f4b32aec38da2caf51
Doug Goldstein <doug.goldstein@rackspace.com>
2024-04-11 12:38:04 -0500

    feat: update gitops-deploy script to template helm configs
    Updated the gitops-deploy script to add templating for the helm configs
    files. Renamed some variables and set them at the top level to hopefully
    make following what this script does a little bit easier.

--- scripts/gitops-deploy.sh
+++ scripts/gitops-deploy.sh
@@ -17,14 +17,17 @@ if [ $# -ne 1 ]; then
     usage
 fi
 
-SCRIPTS_DIR="$(dirname "$0")"
-
 if [ ! -f "$1" ]; then
     echo "Did not get a file with environment variables." >&2
     usage
 fi
 
 source "$1"
+
+if [ ! -d "${UC_REPO}" ]; then
+    echo "UC_REPO not set to a path." >&2
+    usage
+fi
 
 if [ ! -d "${UC_DEPLOY}" ]; then
     echo "UC_DEPLOY not set to a path." >&2
@@ -36,7 +39,10 @@ if [ "x${DEPLOY_NAME}" = "x" ]; then
     usage
 fi
 
-OUTPUT_DIR="${UC_DEPLOY}/clusters/${DEPLOY_NAME}"
+UC_REPO_APPS="${UC_REPO}/apps"
+UC_REPO_COMPONENTS="${UC_REPO}/components"
+UC_DEPLOY_CLUSTER="${UC_DEPLOY}/clusters/${DEPLOY_NAME}"
+UC_DEPLOY_HELM_CFG="${UC_DEPLOY}/helm-configs/${DEPLOY_NAME}"
 
 export DNS_ZONE
 export UC_DEPLOY_GIT_URL
@@ -45,23 +51,28 @@ export UC_REPO_REF="${UC_REPO_REF:-HEAD}"
 
 for part in operators components; do
     echo "Creating ${part} configs"
-    mkdir -p "${OUTPUT_DIR}/${part}"
-    for tmpl in $(find "${SCRIPTS_DIR}/../apps/${part}" -type f); do
+    mkdir -p "${UC_DEPLOY_CLUSTER}/${part}"
+    for tmpl in $(find "${UC_REPO_APPS}/${part}" -type f); do
         outfile=$(basename "${tmpl}")
-        template "${tmpl}" "${OUTPUT_DIR}/${part}/${outfile}"
+        template "${tmpl}" "${UC_DEPLOY_CLUSTER}/${part}/${outfile}"
     done
-    rm -rf "${OUTPUT_DIR}/${part}/kustomization.yaml"
+    rm -rf "${UC_DEPLOY_CLUSTER}/${part}/kustomization.yaml"
 done
 
 # create helm-configs directory for values.yaml overrides
-helmcfgsdir="${UC_DEPLOY}/helm-configs/${DEPLOY_NAME}"
-mkdir -p "${helmcfgsdir}"
-for helmvals in keystone; do
-    helmvals="${helmcfgsdir}/${helmvals}.yaml"
-    if [ ! -f "${helmvals}" ]; then
+mkdir -p "${UC_DEPLOY_HELM_CFG}"
+for component in keystone dexidp; do
+    helmvals="${UC_DEPLOY_HELM_CFG}/${component}.yaml"
+    if [ -f "${helmvals}" ]; then
+        echo "You have ${helmvals} already, not overwriting"
+        continue
+    fi
+    if  [ -f "${UC_REPO_COMPONENTS}/${component}/values.tpl.yaml" ]; then
+            template "${UC_REPO_COMPONENTS}/${component}/values.tpl.yaml" "${helmvals}"
+    else
         echo "# add your values.yaml overrides for the helm chart here" > "${helmvals}"
     fi
 done
 
 echo "Creating app-of-apps config"
-template "${SCRIPTS_DIR}/../apps/app-of-apps.yaml" "${OUTPUT_DIR}/app-of-apps.yaml"
+template "${UC_REPO_APPS}/app-of-apps.yaml" "${UC_DEPLOY_CLUSTER}/app-of-apps.yaml"
