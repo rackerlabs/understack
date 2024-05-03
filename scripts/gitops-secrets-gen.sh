2b6c1a109ef17f22ee6546e3d785cb4e9555abd0
Doug Goldstein <doug.goldstein@rackspace.com>
2024-05-03 18:02:29 -0500

    feat: make running gitops secrets gen from another box easier
    Make it possible to re-run the secrets generating script when you don't
    have the configs for setting up the repo access locally. This should
    make it easier to share things between folks.

--- scripts/gitops-secrets-gen.sh
+++ scripts/gitops-secrets-gen.sh
@@ -30,19 +30,21 @@ if [ "x${DEPLOY_NAME}" = "x" ]; then
     usage
 fi
 
-if [ "x${UC_DEPLOY_GIT_URL}" = "x" ]; then
-    echo "UC_DEPLOY_GIT_URL is not set." >&2
-    usage
-fi
-
-if [ "x${UC_DEPLOY_SSH_FILE}" = "x" ]; then
-    echo "UC_DEPLOY_SSH_FILE is not set." >&2
-    usage
-fi
-
-if [ ! -f "${UC_DEPLOY_SSH_FILE}" ]; then
-    echo "UC_DEPLOY_SSH_FILE is not a file." >&2
-    usage
+if [ -f "${UC_DEPLOY}/secrets/${DEPLOY_NAME}/argocd/secret-deploy-repo.yaml" ]; then
+    NO_SECRET_DEPLOY=1
+else
+    if [ "x${UC_DEPLOY_GIT_URL}" = "x" ]; then
+        echo "UC_DEPLOY_GIT_URL is not set." >&2
+        usage
+    fi
+    if [ "x${UC_DEPLOY_SSH_FILE}" = "x" ]; then
+        echo "UC_DEPLOY_SSH_FILE is not set." >&2
+        usage
+    fi
+    if [ ! -f "${UC_DEPLOY_SSH_FILE}" ]; then
+        echo "UC_DEPLOY_SSH_FILE at ${UC_DEPLOY_SSH_FILE} does not exist." >&2
+        usage
+    fi
 fi
 
 if [ "x${DNS_ZONE}" = "x" ]; then
@@ -62,9 +64,10 @@ export DO_TMPL_VALUES=y
 mkdir -p "${UC_DEPLOY}/secrets/${DEPLOY_NAME}"
 "${SCRIPTS_DIR}/easy-secrets-gen.sh" "${UC_DEPLOY}/secrets/${DEPLOY_NAME}"
 
-echo "Creating ArgoCD config"
-mkdir -p "${UC_DEPLOY}/secrets/${DEPLOY_NAME}/argocd"
-cat << EOF > "${UC_DEPLOY}/secrets/${DEPLOY_NAME}/argocd/secret-deploy-repo.yaml"
+if [ "x${NO_SECRET_DEPLOY}" = "x" ]; then
+    echo "Creating ArgoCD config"
+    mkdir -p "${UC_DEPLOY}/secrets/${DEPLOY_NAME}/argocd"
+    cat << EOF > "${UC_DEPLOY}/secrets/${DEPLOY_NAME}/argocd/secret-deploy-repo.yaml"
 apiVersion: v1
 kind: Secret
 metadata:
@@ -76,6 +79,7 @@ data:
   type: $(printf "git" | base64)
   url: $(printf "${UC_DEPLOY_GIT_URL}" | base64)
 EOF
+fi
 
 echo "Creating Cert Manager Cluster Issuer"
 cat << EOF > "${UC_DEPLOY}/secrets/${DEPLOY_NAME}/cluster-issuer.yaml"
