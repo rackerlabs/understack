00a9b20565fd0263206b7c048e475e34e48ad705
nicholaskuechler <nicholas.kuechler@rackspace.com>
2024-06-28 15:13:34 -0500

    feat: Adds OpenStack Placement
    Adds OpenStack Placement to be deployed for Nova.

--- scripts/gitops-secrets-gen.sh
+++ scripts/gitops-secrets-gen.sh
@@ -193,6 +193,10 @@ export NOVA_KEYSTONE_PASSWORD="$(./scripts/pwgen.sh)"
 export NOVA_DB_PASSWORD="$(./scripts/pwgen.sh)"
 # rabbitmq user password for the inovaronic queues
 export NOVA_RABBITMQ_PASSWORD="$(./scripts/pwgen.sh)"
+# placement keystone service account
+export PLACEMENT_KEYSTONE_PASSWORD="$(./scripts/pwgen.sh)"
+# placement user password in mariadb for placement db
+export PLACEMENT_DB_PASSWORD="$(./scripts/pwgen.sh)"
 
 [ ! -f "${DEST_DIR}/secret-keystone-rabbitmq-password.yaml" ] && \
 kubectl --namespace openstack \
@@ -290,6 +294,22 @@ kubectl --namespace openstack \
     --from-literal=username="nova" \
     --from-literal=password="${NOVA_KEYSTONE_PASSWORD}" \
     --dry-run=client -o yaml | secret-seal-stdin "${DEST_DIR}/secret-nova-keystone-password.yaml"
+
+# placement credentials
+[ ! -f "${DEST_DIR}/secret-placement-keystone-password.yaml" ] && \
+kubectl --namespace openstack \
+    create secret generic placement-keystone-password \
+    --type Opaque \
+    --from-literal=username="placement" \
+    --from-literal=password="${PLACEMENT_KEYSTONE_PASSWORD}" \
+    --dry-run=client -o yaml | secret-seal-stdin "${DEST_DIR}/secret-placement-keystone-password.yaml"
+
+[ ! -f "${DEST_DIR}/secret-placement-db-password.yaml" ] && \
+kubectl --namespace openstack \
+    create secret generic placement-db-password \
+    --type Opaque \
+    --from-literal=password="${PLACEMENT_DB_PASSWORD}" \
+    --dry-run=client -o yaml | secret-seal-stdin "${DEST_DIR}/secret-placement-db-password.yaml"
 
 if [ "x${DO_TMPL_VALUES}" = "xy" ]; then
     [ ! -f "${DEST_DIR}/secret-openstack.yaml" ] && \
