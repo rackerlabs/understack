b7149149b4c67a31317409042b18ac24e88ac4e7
Andrew Harris <andrew.harris@rackspace.com>
2024-07-02 16:59:09 -0400

    feat: created a workflow which can be used to sync Nautobot Interfaces to Ironic Ports

--- argo-workflows/ironic-nautobot-sync/code/ironic/client.py
+++ argo-workflows/ironic-nautobot-sync/code/ironic/client.py
@@ -1,6 +1,7 @@
 from ironicclient import client as iclient
 from keystoneauth1 import session
 from keystoneauth1.identity import v3
+from typing import List
 
 
 class IronicClient:
@@ -30,9 +31,7 @@ class IronicClient:
             user_domain_name="Default",
         )
         insecure_ssl = True
-        sess = session.Session(
-            auth=auth, verify=(not insecure_ssl), app_name="nautobot"
-        )
+        sess = session.Session(auth=auth, verify=(not insecure_ssl), app_name="nautobot")
         self.client = iclient.Client(
             1,
             endpoint_override=self.svc_url,
@@ -45,9 +44,7 @@ class IronicClient:
     def create_node(self, node_data: dict):
         self._ensure_logged_in()
 
-        return self.client.node.create(
-            os_ironic_api_version=self.os_ironic_api_version, **node_data
-        )
+        return self.client.node.create(os_ironic_api_version=self.os_ironic_api_version, **node_data)
 
     def list_nodes(self):
         self._ensure_logged_in()
@@ -63,6 +60,26 @@ class IronicClient:
         self._ensure_logged_in()
 
         return self.client.node.update(node_id, patch, os_ironic_api_version=self.os_ironic_api_version)
+
+    def create_port(self, port_data: dict):
+        self._ensure_logged_in()
+
+        return self.client.port.create(os_ironic_api_version=self.os_ironic_api_version, **port_data)
+
+    def update_port(self, port_id: str, patch: List):
+        self._ensure_logged_in()
+
+        return self.client.port.update(port_id, patch, os_ironic_api_version=self.os_ironic_api_version)
+
+    def delete_port(self, port_id: str):
+        self._ensure_logged_in()
+
+        return self.client.port.delete(port_id, os_ironic_api_version=self.os_ironic_api_version)
+
+    def list_ports(self, node_id: dict):
+        self._ensure_logged_in()
+
+        return self.client.port.list(node=node_id, detail=True)
 
     def _ensure_logged_in(self):
         if not self.logged_in:
