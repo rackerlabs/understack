55b428e1679c476da0307f9956d3de7b95608f4b
Doug Goldstein <doug.goldstein@rackspace.com>
2024-07-29 12:40:25 -0500

    chore: move sync-nb-server-to-ironic into python package
    Moved the sync-nb-server-to-ironic code into the our workflows python
    package. This required fixing up some places to craft the necessary
    entry points as well as setting dependencies. Removed the building of
    the container for the old directory and create a new container with
    these pieces.

---
+++ python/understack-workflows/understack_workflows/main/synchronize_server.py
@@ -0,0 +1,98 @@
+import json
+import logging
+import sys
+
+import ironicclient.common.apiclient.exceptions
+
+from understack_workflows.ironic.client import IronicClient
+from understack_workflows.ironic.secrets import read_secret
+from understack_workflows.node_configuration import IronicNodeConfiguration
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+
+def replace_or_add_field(path, current_val, expected_val):
+    if current_val == expected_val:
+        return None
+    if current_val is None:
+        return {"op": "add", "path": path, "value": expected_val}
+    else:
+        return {"op": "replace", "path": path, "value": expected_val}
+
+
+def event_to_node_configuration(event: dict) -> IronicNodeConfiguration:
+    node_config = IronicNodeConfiguration()
+    node_config.conductor_group = None
+    node_config.driver = "redfish"
+    node_config.chassis_uuid = None
+    node_config.uuid = event["device"]["id"]
+    node_config.name = event["device"]["name"]
+
+    return node_config
+
+
+def main():
+    if len(sys.argv) < 1:
+        raise ValueError(
+            "Please provide node configuration in JSON format as first argument."
+        )
+
+    logger.info("Pushing device new node to Ironic.")
+    client = IronicClient(
+        svc_url=read_secret("IRONIC_SVC_URL"),
+        username=read_secret("IRONIC_USERNAME"),
+        password=read_secret("IRONIC_PASSWORD"),
+        auth_url=read_secret("IRONIC_AUTH_URL"),
+        tenant_name=read_secret("IRONIC_TENANT"),
+    )
+
+    interface_update_event = json.loads(sys.argv[1])
+    logger.debug(f"Received: {interface_update_event}")
+    update_data = interface_update_event["data"]
+
+    node_id = update_data["device"]["id"]
+    logger.debug(f"Checking if node with UUID: {node_id} exists in Ironic.")
+
+    try:
+        ironic_node = client.get_node(node_id)
+    except ironicclient.common.apiclient.exceptions.NotFound:
+        logger.debug(f"Node: {node_id} not found in Ironic.")
+        ironic_node = None
+
+    if not ironic_node:
+        node_config = event_to_node_configuration(update_data)
+        response = client.create_node(node_config.create_arguments())
+        logger.debug(response)
+        ironic_node = client.get_node(node_id)
+
+    STATES_ALLOWING_UPDATES = ["enroll"]
+    if ironic_node.provision_state not in STATES_ALLOWING_UPDATES:
+        logger.info(
+            f"Device {node_id} is in a {ironic_node.provision_state} "
+            f"provisioning state, so the updates are not allowed."
+        )
+        sys.exit(0)
+
+    drac_ip = update_data["ip_addresses"][0]["host"]
+    expected_address = f"https://{drac_ip}"
+    current_address = ironic_node.driver_info.get("redfish_address", None)
+    current_verify_ca = ironic_node.driver_info.get("redfish_verify_ca", None)
+
+    patches = [
+        replace_or_add_field(
+            "/driver_info/redfish_address", current_address, expected_address
+        ),
+        replace_or_add_field(
+            "/driver_info/redfish_verify_ca", current_verify_ca, False
+        ),
+    ]
+    patches = [p for p in patches if p is not None]
+
+    response = client.update_node(node_id, patches)
+    logger.info(f"Patching: {patches}")
+    logger.info(f"Updated: {response}")
