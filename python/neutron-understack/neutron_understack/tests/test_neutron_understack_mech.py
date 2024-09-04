32249a533228619e58c7368de1d334aadfc75a4c
Steve Keay <steve.keay@rackspace.co.uk>
2024-09-04 12:41:34 +0100

    Add basic tests and linting

--- python/neutron-understack/neutron_understack/tests/test_neutron_understack_mech.py
+++ python/neutron-understack/neutron_understack/tests/test_neutron_understack_mech.py
@@ -1,37 +1,34 @@
-from dataclasses import dataclass
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
+
 import pytest
+
+from neutron_understack import neutron_understack_mech
 from neutron_understack.argo.workflows import ArgoClient
 
-from neutron_understack.neutron_understack_mech import UnderstackDriver
 
-# TODO: I am not sure how we run tests in this project.
+@pytest.fixture
+def argo_client() -> ArgoClient:
+    return MagicMock(spec_set=ArgoClient)
 
-@dataclass
-class ContextDouble:
-  current: dict
 
-def mock_context_data(file):
-    ref = pathlib.Path(__file__).joinpath(filename)
-    with ref.open("r") as f:
-        return ContextDouble(json.load(f))
-
-@patch('neutron_understack.argo.workflows.ArgoClient')
-def test_update_port_postcommit(mock_argo_client):
-    context_data = mock_context_data(
-        "fixtures/neutron_update_port_postcommit.json"
+def test_move_to_network__provisioning(argo_client):
+    neutron_understack_mech._move_to_network(
+        vif_type="other",
+        mac_address="fa:16:3e:35:1c:3d",
+        device_uuid="41d18c6a-5548-4ee9-926f-4e3ebf43153f",
+        network_id="c2702769-5592-4555-8ae6-e670db82c31e",
+        argo_client=argo_client,
     )
 
-    UnderstackDriver.update_port_postcommit(context_data)
-
-    mock_argo_client.assert_called_once_with(
+    argo_client.submit.assert_called_once_with(
         template_name="undersync-device",
         entrypoint="trigger-undersync",
         parameters={
-            "interface_uuid": "e5d5cd73-ca9a-4b74-9d52-43188d0cdcaa",
+            "interface_mac": "fa:16:3e:35:1c:3d",
             "device_uuid": "41d18c6a-5548-4ee9-926f-4e3ebf43153f",
-            "network_name": "provisioning",
-            "dry_run": false,
-            "force": false,
+            "network_name": "tenant",
+            "dry_run": True,
+            "force": False,
         },
+        service_account="workflow",
     )
