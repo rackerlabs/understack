dfd3b524c591d50c0b237fc7eae9a7548f4b41f8
nicholaskuechler <nicholas.kuechler@rackspace.com>
2024-09-17 15:21:20 -0500

    fix: fix neutron-understack test

--- python/neutron-understack/neutron_understack/tests/test_neutron_understack_mech.py
+++ python/neutron-understack/neutron_understack/tests/test_neutron_understack_mech.py
@@ -28,6 +28,7 @@ def test_move_to_network__provisioning(argo_client):
             "interface_mac": "fa:16:3e:35:1c:3d",
             "device_uuid": "41d18c6a-5548-4ee9-926f-4e3ebf43153f",
             "network_name": "tenant",
+            "network_id": "c2702769-5592-4555-8ae6-e670db82c31e",
             "dry_run": True,
             "force": False,
         },
