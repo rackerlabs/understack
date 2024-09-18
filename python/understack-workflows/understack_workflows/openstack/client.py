92533beebc91856e8ea2ec396a897ea815db25c7
Doug Goldstein <doug.goldstein@rackspace.com>
2024-09-18 10:59:12 -0500

    feat(understack-workflow): use clouds.yaml for auth
    Provided a wrapper so that we can use clouds.yaml for authentication
    from all of our workflows. Dropped the API version override from Ironic
    as it conflicted with how negotiation works. Negiotation sets a minimum
    and a maximum range based on server and client support. 1.82 is older
    than the client and the server support so it failed. Previous
    negotiation when done through the manually configured client resulted in
    a range of 1.1 to 1.90, which is why we needed to pass 1.82 to get new
    enough micro API. But this client results in 1.88 being used.

---
+++ python/understack-workflows/understack_workflows/openstack/client.py
@@ -0,0 +1,56 @@
+"""helper to setup OpenStack clients."""
+
+# attempt to prevent re-export
+import os as _os
+import sys as _sys
+from importlib import metadata as _meta
+
+from ironicclient.client import Client as IronicClient
+from ironicclient.client import get_client as _get_ironic_client
+from openstack import config as _os_config
+from openstack.connection import Connection
+
+try:
+    _pkg_ver = _meta.version(__package__.split(".")[0])
+except Exception:
+    _pkg_ver = "dev"
+
+try:
+    _prog_name = _os.path.basename(_sys.argv[0]) or "local"
+except Exception:
+    _prog_name = "local"
+
+
+def _get_os_cloud_region(cloud=None, region_name=""):
+    """Returns a keystoneauth1 Session based on our clouds.yaml."""
+    return _os_config.get_cloud_region(
+        load_yaml_config=True,
+        load_envvars=True,
+        app_name=_prog_name,
+        app_version=_pkg_ver,
+        cloud=cloud,
+        region_name=region_name,
+    )
+
+
+def get_openstack_client(cloud=None, region_name="") -> Connection:
+    """Returns an OpenStackSDK Connection based on our clouds.yaml."""
+    cloud_region = _get_os_cloud_region(cloud, region_name)
+
+    return Connection(config=cloud_region)
+
+
+def get_ironic_client(cloud=None, region_name="") -> IronicClient:
+    """Returns our Ironic Client wrapper configured from our clouds.yaml."""
+    cloud_region = _get_os_cloud_region(cloud, region_name)
+    client = _get_ironic_client(
+        "1", session=cloud_region.get_session(), os_ironic_api_version="latest"
+    )
+    client.negotiate_api_version()
+    return client
+
+
+__all__ = [
+    "get_openstack_client",
+    "get_ironic_client",
+]
