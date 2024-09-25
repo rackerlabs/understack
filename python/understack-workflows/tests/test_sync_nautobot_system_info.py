156efe3f8579a13a98d66f75559efcdfe605f36a
Steve Keay <steve.keay@rackspace.co.uk>
2024-09-25 13:39:30 +0100

    Update "sync system" workflow to use standard password, not credential

--- python/understack-workflows/tests/test_sync_nautobot_system_info.py
+++ python/understack-workflows/tests/test_sync_nautobot_system_info.py
@@ -7,35 +7,20 @@ from understack_workflows.main.sync_nautobot_system_info import argument_parser
 def fakebot(mocker):
     return mocker.patch("understack_workflows.nautobot.Nautobot", autospec=True)
 
+@pytest.fixture
+def mock_creds(mocker):
+    mock = mocker.patch("understack_workflows.node_configuration.credential")
+    mock.return_value = "ultra-secret credential value"
+    return mock
 
-def test_parse_device_name():
+def test_parse_device_name(mock_creds):
     parser = argument_parser(__name__)
     with pytest.raises(SystemExit):
-        parser.parse_args(
-            [
-                "--device-id",
-                "FOO",
-                "--bmc_username",
-                "root",
-                "--bmc_password",
-                "password",
-            ]
-        )
+        parser.parse_args(["--device-id", "FOO"])
 
 
-def test_parse_device_id(device_id, bmc_username, bmc_password):
+def test_parse_device_id(device_id, mock_creds):
     parser = argument_parser(__name__)
-    args = parser.parse_args(
-        [
-            "--device-id",
-            str(device_id),
-            "--bmc_username",
-            bmc_username,
-            "--bmc_password",
-            bmc_password,
-        ]
-    )
+    args = parser.parse_args(["--device-id", str(device_id)])
 
     assert args.device_id == device_id
-    assert args.bmc_username == bmc_username
-    assert args.bmc_password == bmc_password
