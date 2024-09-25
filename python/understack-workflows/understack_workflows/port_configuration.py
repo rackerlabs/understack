1160ab66e9c25e22dc35c6c4febad525b13d3a80
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-09-25 09:15:20 +0100

    sync_interfaces: make the port names unique

--- python/understack-workflows/understack_workflows/port_configuration.py
+++ python/understack-workflows/understack_workflows/port_configuration.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from pydantic import BaseModel
 from pydantic import StringConstraints
+from pydantic import field_serializer
 
 
 class PortConfiguration(BaseModel):
@@ -11,3 +12,8 @@ class PortConfiguration(BaseModel):
     uuid: str  # using a str here to due to ironicclient Port attribute
     node_uuid: str  # using a str here due to ironicclient Port attribute
     name: str  # port name
+
+    # Ironic requires the port names to be globally unique
+    @field_serializer('name')
+    def serialize_name(self, name: str):
+        return f"{self.uuid} {name}"
