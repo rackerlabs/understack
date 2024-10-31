50979b5fa9d96874b672a756cc472846a4da781a
Steve Keay <steve.keay@rackspace.co.uk>
2024-10-31 11:07:35 +0000

    Refactor: Parse Nautobot data into a dataclass instead of a dict

--- python/understack-workflows/understack_workflows/sync_interfaces.py
+++ python/understack-workflows/understack_workflows/sync_interfaces.py
@@ -2,26 +2,29 @@ from ironicclient.v1.port import Port
 
 from understack_workflows.helpers import setup_logger
 from understack_workflows.ironic.client import IronicClient
+from understack_workflows.nautobot_device import NautobotDevice
+from understack_workflows.nautobot_device import NautobotInterface
 from understack_workflows.port_configuration import PortConfiguration
 
 logger = setup_logger(__name__)
 
 
 def from_nautobot_to_ironic(
-    nautobot_data: dict, pxe_interface: str, ironic_client=None
+    nautobot_device: NautobotDevice, pxe_interface: str, ironic_client=None
 ):
     """Update Ironic ports to match information found in Nautobot Interfaces."""
-    device_id = nautobot_data["id"]
-    logger.info(f"Syncing Interfaces / Ports for Device {device_id} ...")
+    logger.info(f"Syncing Interfaces / Ports for Device {nautobot_device.id} ...")
 
-    nautobot_ports = dict_by_uuid(get_nautobot_interfaces(nautobot_data, pxe_interface))
+    nautobot_ports = dict_by_uuid(
+        get_nautobot_interfaces(nautobot_device, pxe_interface)
+    )
     logger.info(f"{nautobot_ports}")
 
     if ironic_client is None:
         ironic_client = IronicClient()
 
     logger.info("Fetching Ironic Ports ...")
-    ironic_ports = dict_by_uuid(ironic_client.list_ports(device_id))
+    ironic_ports = dict_by_uuid(ironic_client.list_ports(nautobot_device.id))
 
     for port_id, interface in ironic_ports.items():
         if port_id not in nautobot_ports:
@@ -52,7 +55,7 @@ def dict_by_uuid(items: list) -> dict:
 
 
 def get_nautobot_interfaces(
-    nautobot_data, pxe_interface: str
+    nautobot_device: NautobotDevice, pxe_interface: str
 ) -> list[PortConfiguration]:
     """Get Nautobot interfaces for a device.
 
@@ -61,33 +64,36 @@ def get_nautobot_interfaces(
     Excludes interfaces with no MAC address
 
     """
-    device_id = nautobot_data["id"]
+    device_id = nautobot_device.id
 
     return [
         port_configuration(interface, pxe_interface, device_id)
-        for interface in nautobot_data["interfaces"]
+        for interface in nautobot_device.interfaces
         if interface_is_relevant(interface)
     ]
 
 
-def port_configuration(interface: dict, pxe_interface, device_id) -> PortConfiguration:
+def port_configuration(
+    interface: NautobotInterface, pxe_interface: str, device_id: str
+) -> PortConfiguration:
     # Interface names have their UUID prepended because Ironic wants them
     # globally unique across all devices.
-    name = f"{interface['id']} {interface['name']}"
-    pxe_enabled = interface["name"] == pxe_interface
+    name = f"{interface.id} {interface.name}"
+    pxe_enabled = interface.name == pxe_interface
 
     return PortConfiguration(
         node_uuid=device_id,
-        address=interface["mac_address"],
-        uuid=interface["id"],
+        address=interface.mac_address.lower(),
+        uuid=interface.id,
         name=name,
         pxe_enabled=pxe_enabled,
     )
 
 
-def interface_is_relevant(interface: dict) -> bool:
-    name = interface["name"]
-    return interface["mac_address"] and name != "iDRAC" and name != "iLo"
+def interface_is_relevant(interface: NautobotInterface) -> bool:
+    return bool(
+        interface.mac_address and interface.name != "iDRAC" and interface.name != "iLo"
+    )
 
 
 def get_patch(nautobot_port: PortConfiguration, port: Port) -> list:
