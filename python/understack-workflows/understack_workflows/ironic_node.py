ce6645156a26d738d7a3cb8c4d95887b493ef046
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-11-14 16:13:05 +0000

    enroll: wire in flavor matching code

--- python/understack-workflows/understack_workflows/ironic_node.py
+++ python/understack-workflows/understack_workflows/ironic_node.py
@@ -12,31 +12,37 @@ STATES_ALLOWING_UPDATES = ["enroll", "manageable"]
 logger = setup_logger(__name__)
 
 
-def create_or_update(
-    node_uuid: str, device_hostname: str, device_manufacturer: str, bmc: Bmc, logger
-):
+@dataclass(frozen=True)
+class NodeMetadata:
+    uuid: str
+    hostname: str
+    manufacturer: str
+    resource_class: str
+
+    @property
+    def driver(self):
+        if self.manufacturer.startswith("Dell"):
+            return "idrac"
+        else:
+            return "redfish"
+
+
+def create_or_update(node_meta: NodeMetadata, bmc: Bmc):
     """Note interfaces/ports are not synced here, that happens elsewhere."""
     client = IronicClient()
-    if device_manufacturer.startswith("Dell"):
-        driver = "idrac"
-    else:
-        driver = "redfish"
-
-    logger.debug(f"Ensuring node with UUID {node_uuid} exists in Ironic")
+    logger.debug(f"Ensuring node with UUID {node_meta.uuid} exists in Ironic")
     try:
-        ironic_node = client.get_node(node_uuid)
+        ironic_node = client.get_node(node_meta.uuid)
     except ironicclient.common.apiclient.exceptions.NotFound:
-        logger.debug(f"Node: {node_uuid} not found in Ironic, creating.")
-        ironic_node = create_ironic_node(
-            client, node_uuid, device_hostname, driver, bmc
-        )
+        logger.debug(f"Node: {node_meta.uuid} not found in Ironic, creating.")
+        ironic_node = create_ironic_node(client, node_meta, bmc)
         return ironic_node.provision_state  # type: ignore
 
     if ironic_node.provision_state in STATES_ALLOWING_UPDATES:
-        update_ironic_node(client, node_uuid, device_hostname, driver, bmc)
+        update_ironic_node(client, node_meta, bmc)
     else:
         logger.info(
-            f"Device {node_uuid} in Ironic is in a "
+            f"Device {node_meta.uuid} in Ironic is in a "
             f"{ironic_node.provision_state} provision_state, "
             f"so no updates are allowed."
         )
@@ -44,40 +50,40 @@ def create_or_update(
     return ironic_node.provision_state
 
 
-def update_ironic_node(client, node_uuid, device_hostname, driver, bmc):
+def update_ironic_node(client, node_meta, bmc):
     updates = [
-        f"name={device_hostname}",
-        f"driver={driver}",
+        f"name={node_meta.hostname}",
+        f"driver={node_meta.driver}",
         f"driver_info/redfish_address={bmc.url()}",
         "driver_info/redfish_verify_ca=false",
         f"driver_info/redfish_username={bmc.username}",
         f"driver_info/redfish_password={bmc.password}",
+        f"resource_class={node_meta.resource_class}",
     ]
 
     patches = args_array_to_patch("add", updates)
-    logger.info(f"Updating Ironic node {node_uuid} {patches=}")
+    logger.info(f"Updating Ironic node {node_meta.uuid} {patches=}")
 
-    response = client.update_node(node_uuid, patches)
-    logger.info(f"Ironic node {node_uuid} Updated: {response=}")
+    response = client.update_node(node_meta.uuid, patches)
+    logger.info(f"Ironic node {node_meta.uuid} Updated: {response=}")
 
 
 def create_ironic_node(
     client: IronicClient,
-    node_uuid: str,
-    device_hostname: str,
-    driver: str,
+    node_meta: NodeMetadata,
     bmc: Bmc,
 ) -> IronicNodeConfiguration:
     return client.create_node(
         {
-            "uuid": node_uuid,
-            "name": device_hostname,
-            "driver": driver,
+            "uuid": node_meta.uuid,
+            "name": node_meta.hostname,
+            "driver": node_meta.driver,
             "driver_info": {
                 "redfish_address": bmc.url(),
                 "redfish_verify_ca": False,
                 "redfish_username": bmc.username,
                 "redfish_password": bmc.password,
             },
+            "resource_class": node_meta.resource_class,
         }
     )
