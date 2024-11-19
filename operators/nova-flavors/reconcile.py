7aa2ad2326a72fa58aceb01b6768f05ba3286d52
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-11-19 16:22:46 +0000

    nova_flavors: watch filesystem for modifications

--- operators/nova-flavors/reconcile.py
+++ operators/nova-flavors/reconcile.py
@@ -1,23 +1,51 @@
+import logging
+import os
+import time
+
 from flavor_matcher.flavor_spec import FlavorSpec
-from flavor_matcher.flavor_spec import os
+from watchdog.observers import Observer
+
 from flavor_synchronizer import FlavorSynchronizer
 from logger import setup_logger
+from spec_changed_handler import SpecChangedHandler
 
-logger = setup_logger(__name__)
+loglevel = getattr(logging, os.getenv("NOVA_FLAVOR_MONITOR_LOGLEVEL", "info").upper())
+logging.getLogger().setLevel(loglevel)
+logger = setup_logger(__name__, level=loglevel)
 
 # nonprod vs prod
 flv_env = os.getenv("FLAVORS_ENV", "undefined")
-all_flavors = FlavorSpec.from_directory(os.getenv("FLAVORS_DIR", ""))
-defined_flavors = [flv for flv in all_flavors if flv.name.startswith(flv_env)]
+FLAVORS_DIR = os.getenv("FLAVORS_DIR", "")
+if not os.path.isdir(FLAVORS_DIR):
+    raise ValueError(f"FLAVORS_DIR '{FLAVORS_DIR}' is not a directory")
 
 
+def defined_flavors():
+    all_flavors = FlavorSpec.from_directory(FLAVORS_DIR)
+    return [flv for flv in all_flavors if flv.name.startswith(flv_env)]
 
-synchronizer = FlavorSynchronizer(
-    username=os.environ.get("OS_USERNAME", ""),
-    password=os.environ.get("OS_PASSWORD", ""),
-    project_id=os.environ.get("OS_PROJECT_ID"),
-    user_domain_id=os.environ.get("OS_USER_DOMAIN_ID", ""),
-    auth_url=os.environ.get("OS_AUTH_URL"),
-)
-synchronizer.reconcile(defined_flavors)
 
+def main():
+    synchronizer = FlavorSynchronizer(
+        username=os.environ.get("OS_USERNAME", ""),
+        password=os.environ.get("OS_PASSWORD", ""),
+        project_id=os.environ.get("OS_PROJECT_ID"),
+        user_domain_id=os.environ.get("OS_USER_DOMAIN_ID", ""),
+        auth_url=os.environ.get("OS_AUTH_URL"),
+    )
+
+    handler = SpecChangedHandler(synchronizer, defined_flavors)
+    observer = Observer()
+    observer.schedule(handler, FLAVORS_DIR, recursive=True)
+    logger.info(f"Watching for changes in {FLAVORS_DIR}")
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
+
+if __name__ == "__main__":
+    main()
---
+++ operators/nova-flavors/spec_changed_handler.py
@@ -0,0 +1,37 @@
+
+from watchdog.events import DirModifiedEvent
+from typing import Callable
+from watchdog.events import FileModifiedEvent
+from watchdog.events import FileSystemEventHandler
+from flavor_synchronizer import FlavorSynchronizer
+import time
+
+from logger import setup_logger
+
+logger = setup_logger(__name__)
+
+class SpecChangedHandler(FileSystemEventHandler):
+    COOLDOWN_SECONDS = 30
+
+    def __init__(
+        self, synchronizer: FlavorSynchronizer, flavors_cback: Callable
+    ) -> None:
+        self.last_call = None
+        self.synchronizer = synchronizer
+        self.flavors_cback = flavors_cback
+
+    def on_modified(self, event: DirModifiedEvent | FileModifiedEvent) -> None:
+        if isinstance(event, DirModifiedEvent):
+            self._run(event)
+
+    def _run(self, event):
+        now = time.time()
+        if not self.last_call:
+            self.last_call = now
+        else:
+            if self.last_call + self.COOLDOWN_SECONDS > now:
+                logger.debug("Cooldown period.")
+                return
+        self.last_call = now
+        logger.info(f"Flavors directory {event.src_path} has changed.")
+        self.synchronizer.reconcile(self.flavors_cback())
