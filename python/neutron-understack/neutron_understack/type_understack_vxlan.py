ce8488d1a1580eba3ea935cb1280febaa09f6c9f
Doug Goldstein <doug.goldstein@rackspace.com>
2025-02-10 10:06:15 -0600

    feat(neutron): make our VXLAN type extend upstream
    Instead of a no-op VXLAN type driver that doesn't support anything or do
    anything, switch to just being an extension of the upstream VXLAN type
    driver. In the future this may morph into its own distinct type so that
    we can support physical network fields on this type but currently that
    is not supported by Neutron. The concept of VXLAN in Neutron is centered
    around the hypervisor/instance use case which does not have multiple
    VRFs per hypervisor so there is no ability to separate out the VXLAN
    networks. But in the physical VXLAN fabrics where different servers can
    be bound to different fabrics we do have that separation.

--- python/neutron-understack/neutron_understack/type_understack_vxlan.py
+++ python/neutron-understack/neutron_understack/type_understack_vxlan.py
@@ -1,45 +1,9 @@
+from neutron.plugins.ml2.drivers import type_vxlan
 from neutron_lib import constants as p_const
-from neutron_lib.plugins.ml2 import api
-from oslo_log import log
-
-LOG = log.getLogger(__name__)
 
 
-class UnderstackVxlanTypeDriver(api.ML2TypeDriver):
-    def __init__(self):
-        """Understack based type driver."""
-        super().__init__()
-        LOG.info("ML2 Understack VXLAN Type initialization complete")
+class UnderstackVxlanTypeDriver(type_vxlan.VxlanTypeDriver):
+    """Manage state for EVPN L2VNI networks with ML2."""
 
     def get_type(self):
         return p_const.TYPE_VXLAN
-
-    def initialize(self):
-        pass
-
-    def initialize_network_segment_range_support(self):
-        pass
-
-    def update_network_segment_range_allocations(self):
-        pass
-
-    def get_network_segment_ranges(self):
-        pass
-
-    def is_partial_segment(self, segment):
-        return False
-
-    def validate_provider_segment(self, segment):
-        pass
-
-    def reserve_provider_segment(self, context, segment, filters=None):
-        return segment
-
-    def allocate_tenant_segment(self, context, filters=None):
-        return {api.NETWORK_TYPE: p_const.TYPE_VXLAN}
-
-    def release_segment(self, context, segment):
-        pass
-
-    def get_mtu(self, physical_network=None):
-        pass
