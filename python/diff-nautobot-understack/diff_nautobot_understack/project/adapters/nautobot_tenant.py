a41be43ad6bf69b778dfef4e627aef27a597c51f
Nicholas Kuechler <nicholas.kuechler@rackspace.com>
2025-03-21 16:57:19 -0500

    feat: Adds 'uc-diff projects' to diff all projects

--- python/diff-nautobot-understack/diff_nautobot_understack/project/adapters/nautobot_tenant.py
+++ python/diff-nautobot-understack/diff_nautobot_understack/project/adapters/nautobot_tenant.py
@@ -35,3 +35,29 @@ class Tenant(Adapter):
                     description=tenant.get("description"),
                 )
             )
+
+
+class Tenants(Adapter):
+    """All tenants."""
+
+    project = models.ProjectModel
+
+    top_level = ["project"]
+    type = "Tenant"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.api_client = API()
+
+    def load(self):
+        url = "/api/tenancy/tenants/?include=relationships"
+        tenants_response = self.api_client.make_api_request(url, paginated=True)
+
+        for tenant in tenants_response:
+            self.add(
+                self.project(
+                    id=_remove_hyphens(tenant.get("id")),
+                    name=tenant.get("name"),
+                    description=tenant.get("description"),
+                )
+            )
