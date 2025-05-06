851a4831ee80d5c527dfb9b63ef10631435c87f2
Abhimanyu Sharma <abhimanyusharma003@gmail.com>
2025-05-06 15:58:34 +0530

    fix: function rename

--- go/understack/cmd/deploy/deploy.go
+++ go/understack/cmd/deploy/deploy.go
@@ -16,24 +16,75 @@ const (
 	deployRepoFlag   = "deploy-repo"
 )
 
+// NewCmdDeploy returns the root "deploy" command for UnderStack deployments.
 func NewCmdDeploy() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "deploy [--deploy-repo UC_DEPLOY] command",
 		Short: "UnderStack deployment",
-		Long:  ``,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// If no subcommand, show help
+			// Show help if no subcommands are provided
 			return cmd.Help()
 		},
-		PersistentPreRunE: preRun,
+		PersistentPreRunE: ensureDeployRepo,
 	}
 
-	setFlags(cmd)
-
+	addDeployRepoFlag(cmd)
 	return cmd
 }
 
-func expandPath(path string) (string, error) {
+// addDeployRepoFlag binds and configures the persistent --deploy-repo flag.
+func addDeployRepoFlag(cmd *cobra.Command) {
+	// Bind environment variable
+	if err := viper.BindEnv(deployRepoFlag, deployRepoEnvVar); err != nil {
+		log.Fatal("failed to bind env var", "env", deployRepoEnvVar, "err", err)
+		os.Exit(1)
+	}
+
+	// Set default value from env or fallback
+	defaultRepo := viper.GetString(deployRepoFlag)
+	if defaultRepo == "" {
+		defaultRepo = "."
+	}
+
+	help := fmt.Sprintf("Path to your deployment repo (env: %s) (current: %s)", deployRepoEnvVar, defaultRepo)
+	cmd.PersistentFlags().String(deployRepoFlag, "", help)
+
+	if err := viper.BindPFlag(deployRepoFlag, cmd.PersistentFlags().Lookup(deployRepoFlag)); err != nil {
+		log.Fatal("failed to bind flag", "flag", deployRepoFlag, "err", err)
+		os.Exit(1)
+	}
+}
+
+// ensureDeployRepo validates and switches to the deployment repo directory, if provided.
+func ensureDeployRepo(cmd *cobra.Command, args []string) error {
+	repoPath := viper.GetString(deployRepoFlag)
+	if repoPath == "" {
+		return nil // nothing to do
+	}
+
+	expandedPath, err := expandHomePath(repoPath)
+	if err != nil {
+		return fmt.Errorf("failed to expand path: %w", err)
+	}
+
+	info, err := os.Stat(expandedPath)
+	if err != nil {
+		return fmt.Errorf("could not access directory '%s': %w", expandedPath, err)
+	}
+	if !info.IsDir() {
+		return fmt.Errorf("path '%s' is not a directory", expandedPath)
+	}
+
+	if err := os.Chdir(expandedPath); err != nil {
+		return fmt.Errorf("failed to change working directory to '%s': %w", expandedPath, err)
+	}
+
+	log.Infof("Using deployment repo: %s", expandedPath)
+	return nil
+}
+
+// expandHomePath expands a path starting with "~" to the user's home directory.
+func expandHomePath(path string) (string, error) {
 	if strings.HasPrefix(path, "~") {
 		home, err := os.UserHomeDir()
 		if err != nil {
@@ -42,54 +93,4 @@ func expandPath(path string) (string, error) {
 		return filepath.Join(home, path[1:]), nil
 	}
 	return path, nil
-}
-
-func preRun(cmd *cobra.Command, args []string) error {
-	deployRepo := viper.GetString(deployRepoFlag)
-	if deployRepo == "" {
-		return nil
-	}
-
-	expandedRepoDir, err := expandPath(deployRepo)
-	if err != nil {
-		return fmt.Errorf("failed to expand path: %w", err)
-	}
-
-	info, err := os.Stat(expandedRepoDir)
-	if err != nil {
-		return fmt.Errorf("could not access directory '%s': %w", expandedRepoDir, err)
-	}
-	if !info.IsDir() {
-		return fmt.Errorf("path '%s' is not a directory", expandedRepoDir)
-	}
-
-	// Switch working directory
-	if err := os.Chdir(expandedRepoDir); err != nil {
-		return fmt.Errorf("failed to change working directory to '%s': %w", expandedRepoDir, err)
-	}
-
-	log.Infof("deployment repo path: %s", expandedRepoDir)
-
-	return nil
-}
-
-func setFlags(cmd *cobra.Command) {
-	// bind our flag
-	if err := viper.BindEnv(deployRepoFlag, deployRepoEnvVar); err != nil {
-		log.Fatal("failed to bind", "env", deployRepoFlag, "err", err)
-		os.Exit(1)
-	}
-	deployRepo := viper.GetString(deployRepoFlag)
-	if deployRepo == "" {
-		deployRepo = "."
-	}
-	helpText := fmt.Sprintf(
-		"Path to your deployment repo (env: %s) (current: %s)",
-		deployRepoEnvVar, deployRepo,
-	)
-	cmd.PersistentFlags().String(deployRepoFlag, "", helpText)
-	if err := viper.BindPFlag(deployRepoFlag, cmd.PersistentFlags().Lookup(deployRepoFlag)); err != nil {
-		log.Fatal("failed to bind", "flag", deployRepoFlag, "err", err)
-		os.Exit(1)
-	}
 }
