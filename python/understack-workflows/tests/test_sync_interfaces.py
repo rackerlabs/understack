1d047d446fb4ddecb97a518675d9c1770ba86091
Doug Goldstein <doug.goldstein@rackspace.com>
2025-07-17 09:21:41 -0500

    feat: make ironic port names node-name:port-name as a standard
    Standardize the ironic port names to be the node-name:port-name since
    they have to be unique in Ironic but we want to include the BMC name to
    make it clear to folks which NIC is which.
    
    ref: PUC-1037

--- python/understack-workflows/tests/test_sync_interfaces.py
+++ python/understack-workflows/tests/test_sync_interfaces.py
@@ -20,7 +20,7 @@ def test_sync_with_no_existing_interfaces(dell_nautobot_device):
             "address": "14:23:f3:f5:25:f1",
             "uuid": "8c28941c-02cd-4aad-9e3f-93c39e08b58a",
             "node_uuid": "a3a2983f-d906-4663-943c-c41ab73c9b62",
-            "name": "8c28941c-02cd-4aad-9e3f-93c39e08b58a NIC.Slot.1-2",
+            "name": f"{dell_nautobot_device.name}:NIC.Slot.1-2",
             "pxe_enabled": False,
             "local_link_connection": {
                 "switch_id": "9c:54:16:f5:ad:27",
@@ -36,7 +36,7 @@ def test_sync_with_no_existing_interfaces(dell_nautobot_device):
             "address": "d4:04:e6:4f:8d:b5",
             "uuid": "39d98f09-3199-40e0-87dc-e5ed6dce78e5",
             "node_uuid": "a3a2983f-d906-4663-943c-c41ab73c9b62",
-            "name": "39d98f09-3199-40e0-87dc-e5ed6dce78e5 NIC.Integrated.1-2",
+            "name": f"{dell_nautobot_device.name}:NIC.Integrated.1-2",
             "pxe_enabled": False,
             "local_link_connection": {
                 "switch_id": "9c:54:16:f5:ac:27",
@@ -52,7 +52,7 @@ def test_sync_with_no_existing_interfaces(dell_nautobot_device):
             "address": "14:23:f3:f5:25:f0",
             "uuid": "7ac587c4-015b-4a0e-b579-91284cbd0406",
             "node_uuid": "a3a2983f-d906-4663-943c-c41ab73c9b62",
-            "name": "7ac587c4-015b-4a0e-b579-91284cbd0406 NIC.Slot.1-1",
+            "name": f"{dell_nautobot_device.name}:NIC.Slot.1-1",
             "pxe_enabled": False,
             "local_link_connection": {
                 "switch_id": "9c:54:16:f5:ad:27",
@@ -68,7 +68,7 @@ def test_sync_with_no_existing_interfaces(dell_nautobot_device):
             "address": "d4:04:e6:4f:8d:b4",
             "uuid": "ac2f1eae-188e-4fc6-9245-f9a6cf8b4ea8",
             "node_uuid": "a3a2983f-d906-4663-943c-c41ab73c9b62",
-            "name": "ac2f1eae-188e-4fc6-9245-f9a6cf8b4ea8 NIC.Integrated.1-1",
+            "name": f"{dell_nautobot_device.name}:NIC.Integrated.1-1",
             "pxe_enabled": False,
             "local_link_connection": {
                 "switch_id": "9c:54:16:f5:ab:27",
