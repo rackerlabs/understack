76ea399dcef068ba0f1fda8065d530e2545d9db6
Doug Goldstein <doug.goldstein@rackspace.com>
2025-08-11 12:55:13 -0500

    more testing

--- python/cinder-understack/cinder_understack/dynamic_netapp_driver.py
+++ python/cinder-understack/cinder_understack/dynamic_netapp_driver.py
@@ -215,24 +215,25 @@ class NetappCinderDynamicDriver(volume_driver.BaseVD):
     def get_volume_stats(self, refresh=False):
         """Get volume stats."""
         if refresh:
-            data = {}
-            data["volume_backend_name"] = (
-                self.configuration.safe_get("volume_backend_name") or self.DRIVER_NAME
-            )
-            data["vendor_name"] = "NetApp"
-            data["driver_version"] = self.VERSION
-            data["storage_protocol"] = "NVMe"
-            data["sparse_copy_volume"] = True
-            data["replication_enabled"] = False
-            data["pools"] = []
-            for svm_name, lib in self._libraries.items():
-                stats = lib.get_volume_stats(
-                    refresh, lib.get_filter_function(), lib.get_goodness_function()
-                )
-                for pool in stats["pools"]:
-                    pool["pool_name"] = f"{svm_name}#{pool['pool_name']}"
-                    data["pools"].append(pool)
-            self._stats = data
+            LOG.INFO("would refresh")
+        #    data = {}
+        #    data["volume_backend_name"] = (
+        #        self.configuration.safe_get("volume_backend_name") or self.DRIVER_NAME
+        #    )
+        #    data["vendor_name"] = "NetApp"
+        #    data["driver_version"] = self.VERSION
+        #    data["storage_protocol"] = "NVMe"
+        #    data["sparse_copy_volume"] = True
+        #    data["replication_enabled"] = False
+        #    data["pools"] = []
+        #    for svm_name, lib in self._libraries.items():
+        #        stats = lib.get_volume_stats(
+        #            refresh, lib.get_filter_function(), lib.get_goodness_function()
+        #        )
+        #        for pool in stats["pools"]:
+        #            pool["pool_name"] = f"{svm_name}#{pool['pool_name']}"
+        #            data["pools"].append(pool)
+        #    self._stats = data
         return self._stats
 
     def update_provider_info(self, volumes, snapshots):
