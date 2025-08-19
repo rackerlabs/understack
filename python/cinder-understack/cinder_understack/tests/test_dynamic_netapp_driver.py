ab773a3a68e314becfaaf7f57fc02f3a1314891f
Doug Goldstein <doug.goldstein@rackspace.com>
2025-08-19 14:13:01 -0500

    chore(cinder-understack): wire up volume and type for testing
    This adds enough boilerplate so that we have a volume type with data set
    and can then utilize it for calling our volume creation function with
    appropriate data.

--- python/cinder-understack/cinder_understack/tests/test_dynamic_netapp_driver.py
+++ python/cinder-understack/cinder_understack/tests/test_dynamic_netapp_driver.py
@@ -1,8 +1,12 @@
 """Test NetApp dynamic driver implementation."""
 
+import uuid
 from unittest import mock
 
+from cinder import context
+from cinder import db
 from cinder.tests.unit import test
+from cinder.tests.unit import utils as test_utils
 from cinder.tests.unit.volume.drivers.netapp import fakes as na_fakes
 from cinder.volume.drivers.netapp.dataontap.nvme_library import NetAppNVMeStorageLibrary
 
@@ -15,6 +19,13 @@ class NetappDynamicDriverTestCase(test.TestCase):
     def setUp(self):
         """Set up test case."""
         super().setUp()
+
+        self.user_id = str(uuid.uuid4())
+        self.project_id = str(uuid.uuid4())
+
+        self.ctxt = context.RequestContext(
+            self.user_id, self.project_id, auth_token=True
+        )
 
         kwargs = {
             "configuration": self.get_config_base(),
@@ -53,8 +64,7 @@ class NetappDynamicDriverTestCase(test.TestCase):
     def test_do_setup_calls_library(self, new_do_setup, old_do_setup, mock_rest):
         """Test that do_setup delegates to library."""
         self._setup_rest_mock(mock_rest)
-        context = mock.Mock()
-        self.driver.do_setup(context)
+        self.driver.do_setup(self.ctxt)
         # not yet wired in
         new_do_setup.assert_not_called()
         old_do_setup.assert_not_called()
@@ -62,9 +72,18 @@ class NetappDynamicDriverTestCase(test.TestCase):
     @mock.patch.object(dynamic_netapp_driver.NetappDynamicLibrary, "create_volume")
     def test_create_volume_calls_library(self, mock_create_volume):
         """Test that create_volume delegates to library."""
-        volume = mock.Mock()
-        self.driver.create_volume(volume)
-        mock_create_volume.assert_called_once_with(volume)
+        ctxt = context.get_admin_context()
+        vol_type = test_utils.create_volume_type(
+            ctxt,
+            self,
+            id=na_fakes.VOLUME.volume_type_id,
+            name="my_vol_type",
+            is_public=False,
+            extra_specs={"netapp:svm_vserver": "data-svm"},
+        )
+        db.volume_type_access_add(ctxt, vol_type.id, self.project_id)
+        self.driver.create_volume(na_fakes.VOLUME)
+        mock_create_volume.assert_called_once_with(na_fakes.VOLUME)
 
     @mock.patch.object(dynamic_netapp_driver.NetappDynamicLibrary, "get_volume_stats")
     def test_get_volume_stats_calls_library(self, mock_get_volume_stats):
