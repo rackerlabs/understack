8d699d415cefb904ade9d853924acb701da63c28
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2025-09-23 18:46:23 +0100

    deliver IPs as NoCloud data source

--- ironic-images/build.sh
+++ ironic-images/build.sh
@@ -1,4 +1,5 @@
 cd /code
 export DIB_RELEASE=bookworm
 export ELEMENTS_PATH=/code/.venv/share/ironic-python-agent-builder/dib:/code/custom_elements
+export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive, OpenStack, NoCloud, None"
 diskimage-builder ./ipa-debian-bookworm.yaml
