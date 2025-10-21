3aed9c7bb17aee4fb2f3e673684cf5a6f0b00a22
Doug Goldstein <doug.goldstein@rackspace.com>
2025-10-21 10:24:51 -0500

    make ironic global

--- python/neutron-understack/neutron_understack/ironic.py
+++ python/neutron-understack/neutron_understack/ironic.py
@@ -4,6 +4,8 @@ from openstack.baremetal.baremetal_service import BaremetalService
 from openstack.baremetal.v1.port import Port as BaremetalPort
 from oslo_config import cfg
 
+IRONIC_SESSION = None
+
 
 class IronicClient:
     def __init__(self):
@@ -15,10 +17,14 @@ class IronicClient:
         return session
 
     def _get_ironic_client(self) -> BaremetalService:
-        session = self._get_session("ironic")
+        global IRONIC_SESSION
+        if not IRONIC_SESSION:
+            IRONIC_SESSION = self._get_session("ironic")
 
         return connection.Connection(
-            session=session, oslo_conf=cfg.CONF, connect_retries=cfg.CONF.http_retries
+            session=IRONIC_SESSION,
+            oslo_conf=cfg.CONF,
+            connect_retries=cfg.CONF.http_retries,
         ).baremetal
 
     def baremetal_port_physical_network(self, local_link_info: dict) -> str | None:
