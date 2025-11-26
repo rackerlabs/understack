3ade42b3e97b8ff52fe9b36323b58a75412fd6f8
Steve Keay <steve.keay@rackspace.co.uk>
2025-11-26 13:54:55 +0000

    Fix baremetmal extra field updating

--- python/ironic-understack/ironic_understack/port_bios_name_hook.py
+++ python/ironic-understack/ironic_understack/port_bios_name_hook.py
@@ -28,10 +28,20 @@ class PortBiosNameHook(base.InspectionHook):
 
         for baremetal_port in ironic_ports_for_node(task.context, task.node.id):
             mac = baremetal_port.address.upper()
-            name = interface_names.get(mac)
-            LOG.info("Port %(mac)s bios_name=%(name)s", {"mac": mac, "name": name})
-            if name:
-                baremetal_port.extra["bios_name"] = name
-            else:
-                baremetal_port.extra.pop("bios_name", None)
-            baremetal_port.save()
+            required_bios_name = interface_names.get(mac)
+            extra = baremetal_port.extra
+            current_bios_name = extra.get("bios_name")
+
+            if current_bios_name != required_bios_name:
+                LOG.debug(
+                    "Port %(mac)s updating bios_name from %(old)s to %(new)s",
+                    {"mac": mac, "old": current_bios_name, "new": required_bios_name}
+                )
+
+                if required_bios_name:
+                    extra["bios_name"] = required_bios_name
+                else:
+                    extra.pop("bios_name", None)
+
+                baremetal_port.extra = extra
+                baremetal_port.save()
