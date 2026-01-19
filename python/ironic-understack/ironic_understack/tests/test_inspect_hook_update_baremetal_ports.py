d58bcc5469a1cc99b179a4f27f180c4410c47827
Steve Keay <steve.keay@rackspace.co.uk>
2026-01-19 15:56:52 +0000

    Fix inspection hook "category" logic in baremetal-ports
    Today we have only two possible categories:  network and storage.
    
    This code was saying "if it isn't network then it is storage" which
    happens to be correct today, but will stop being correct if we add a
    third category.
    
    The category will be set correctly because it is determined from the
    suffix of the "physical network" value, which on a storage port will
    have a value ending in "-storage")

--- python/ironic-understack/ironic_understack/tests/test_inspect_hook_update_baremetal_ports.py
+++ python/ironic-understack/ironic_understack/tests/test_inspect_hook_update_baremetal_ports.py
@@ -63,7 +63,7 @@ MAPPING = {
 }
 
 
-def test_with_valid_data(mocker, caplog):
+def test_with_valid_network_port(mocker, caplog):
     caplog.set_level(logging.DEBUG)
 
     node_uuid = uuidutils.generate_uuid()
@@ -88,10 +88,9 @@ def test_with_valid_data(mocker, caplog):
         "ironic_understack.inspect_hook_update_baremetal_ports.CONF.ironic_understack.switch_name_vlan_group_mapping",
         MAPPING,
     )
-    trait_create = mocker.patch(
+    mocker.patch(
         "ironic_understack.inspect_hook_update_baremetal_ports.objects.TraitList.create"
     )
-
     mock_traits.get_trait_names.return_value = ["CUSTOM_BMC_SWITCH", "bar"]
 
     InspectHookUpdateBaremetalPorts().__call__(mock_task, _INVENTORY, _PLUGIN_DATA)
@@ -104,8 +103,75 @@ def test_with_valid_data(mocker, caplog):
     assert mock_port.physical_network == "f20-3-network"
     assert mock_port.category == "network"
     mock_port.save.assert_called()
-    mock_node.save.assert_called_once()
 
+
+def test_with_valid_storage_port(mocker, caplog):
+    caplog.set_level(logging.DEBUG)
+
+    node_uuid = uuidutils.generate_uuid()
+    mock_traits = mocker.Mock()
+    mock_context = mocker.Mock()
+    mock_node = mocker.Mock(id=1234, traits=mock_traits)
+    mock_task = mocker.Mock(node=mock_node, context=mock_context)
+    mock_port = mocker.Mock(
+        uuid=uuidutils.generate_uuid(),
+        node_id=node_uuid,
+        address="33:33:33:33:33:33",
+        local_link_connection={},
+        physical_network="original_value",
+        category=None,
+    )
+
+    mocker.patch(
+        "ironic_understack.inspect_hook_update_baremetal_ports.ironic_ports_for_node",
+        return_value=[mock_port],
+    )
+    mocker.patch(
+        "ironic_understack.inspect_hook_update_baremetal_ports.CONF.ironic_understack.switch_name_vlan_group_mapping",
+        MAPPING,
+    )
+    mocker.patch(
+        "ironic_understack.inspect_hook_update_baremetal_ports.objects.TraitList.create"
+    )
+    mock_traits.get_trait_names.return_value = ["CUSTOM_BMC_SWITCH", "bar"]
+
+    InspectHookUpdateBaremetalPorts().__call__(mock_task, _INVENTORY, _PLUGIN_DATA)
+
+    assert mock_port.local_link_connection == {
+        "port_id": "Ethernet1/18",
+        "switch_id": "88:5a:92:ec:54:59",
+        "switch_info": "f20-3-1f.iad3.rackspace.net",
+    }
+    assert mock_port.physical_network is None
+    assert mock_port.category == "storage"
+    mock_port.save.assert_called()
+
+
+def test_node_traits_updated(mocker, caplog):
+    caplog.set_level(logging.DEBUG)
+
+    mock_traits = mocker.Mock()
+    mock_context = mocker.Mock()
+    mock_node = mocker.Mock(id=1234, traits=mock_traits)
+    mock_task = mocker.Mock(node=mock_node, context=mock_context)
+
+    mocker.patch(
+        "ironic_understack.inspect_hook_update_baremetal_ports.ironic_ports_for_node",
+        return_value=[],
+    )
+    mocker.patch(
+        "ironic_understack.inspect_hook_update_baremetal_ports.CONF.ironic_understack.switch_name_vlan_group_mapping",
+        MAPPING,
+    )
+    trait_create = mocker.patch(
+        "ironic_understack.inspect_hook_update_baremetal_ports.objects.TraitList.create"
+    )
+
+    mock_traits.get_trait_names.return_value = ["CUSTOM_BMC_SWITCH", "bar"]
+
+    InspectHookUpdateBaremetalPorts().__call__(mock_task, _INVENTORY, _PLUGIN_DATA)
+
+    mock_node.save.assert_called_once()
     trait_create.assert_called_once_with(
         mock_context, 1234, {"CUSTOM_STORAGE_SWITCH", "CUSTOM_NETWORK_SWITCH", "bar"}
     )
