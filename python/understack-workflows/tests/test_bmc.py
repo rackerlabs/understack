7dfaf243b205cd94dcc4db7eca08fdf698172320
Steve Keay <steve.keay@rackspace.co.uk>
2026-04-02 13:33:40 +0100

    Fix test so it is not affected by environment

--- python/understack-workflows/tests/test_bmc.py
+++ python/understack-workflows/tests/test_bmc.py
@@ -12,9 +12,15 @@ def mock_creds(mocker):
     return mock
 
 
-def test_bmc_for_ip_address(mock_creds):
+def test_bmc_for_ip_address(mock_creds, monkeypatch):
+    # The credential function above is overridden by the environment variable so
+    # make sure it is removed, so we are using the correct test credential and
+    # not a production one.
+    monkeypatch.delenv("BMC_MASTER", raising=False)
     assert os.getenv("BMC_MASTER") is None
+
     bmc = bmc_for_ip_address("1.2.3.4")
+
     assert bmc.ip_address == "1.2.3.4"
     assert bmc.url() == "https://1.2.3.4"
     assert bmc.username == "root"
