6806c36f52a5fc80a14abebbc03a22e377af80c8
Kenny Hackworth <khackwor@rackspace.com>
2026-05-14 12:05:30 -0400

    fix: reverting the network driver changes

--- containers/nautobot/nautobot_config.py
+++ containers/nautobot/nautobot_config.py
@@ -183,6 +183,11 @@ PLUGINS_CONFIG = {
         "use_config_context": {
             "connection_options": True,
         },
+        "connection_options": {s": {
+            "netutils_parser": {
+                "cisco_nxos": "cisco_nxos",
+            },
+        },
         "connection_options": {
             "napalm": {
                 "extras": {
@@ -190,11 +195,6 @@ PLUGINS_CONFIG = {
                         "port": 41268,
                     },
                 },
-            },
-        },
-        "network_driver_mappings": {
-            "napalm": {
-                "cisco_nxos": "nxos_ssh",
             },
         },
     },
